8f6d5c67198e16e6700312ec0210b1bfc7fc6529
Ahmed Khaled <ahmed.khaled.yousry@gmail.com>
2024-12-02 23:55:46 +0200

    feat: read excel transactions as a hash table

--- Data-Mining-Project.py
+++ Data-Mining-Project.py
@@ -1,2 +1,18 @@
 import tkinter as tk
 from tkinter import ttk, filedialog
+import pandas as pd
+
+# Can be taken as input from the GUI later on
+minimum_support = 2
+minimum_confidence = 2
+
+transactions_file_path = 'transactions.xlsx'
+transactions_excel = pd.read_excel(transactions_file_path)
+
+transactions_hash_table = {}
+
+# Transform excel input into a horizontal format hash table <TID, Items>
+for _, row in transactions_excel.iterrows():
+    key = row['TiD'] 
+    value = row['items'].split(',')
+    transactions_hash_table[key] = value
